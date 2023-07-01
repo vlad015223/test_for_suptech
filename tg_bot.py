1a2cf9460d27e5b20886ebf8290ada3411d6d93b
Влад Туров <vlad01523@yandex.ru>
2023-07-01 20:38:19 +0300

    update logger info

--- tg_bot.py
+++ tg_bot.py
@@ -3,13 +3,17 @@ import pygsheets
 from aiogram import Bot, Dispatcher, types, executor
 from tokens import TG_TOKEN, SERVICE_FILE
 
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(level=logging.ERROR, filename='errors.log', filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 bot = Bot(token=TG_TOKEN)
 dp = Dispatcher(bot)
 
+# записываем key_id сервисного аккаунта гугл (json файл)
 client = pygsheets.authorize(service_account_file=SERVICE_FILE)
 
+# открываем нужную доку и лист
 spreadsheet = client.open("test_for_suptech")
 worksheet = spreadsheet.worksheet_by_title("Лист1")
 
@@ -29,13 +33,13 @@ async def handle_message(message: types.Message):
                 worksheet.update_values(f"B{index+1}", [[text]])
                 worksheet.update_values(f"C{index+1}", [[formatted_date]])
                 break
-
+        
+        # отправляем юзеру отбивку и ссылку на гуглдок
         await message.reply('Сообщение успешно записано!\nhttps://docs.google.com/spreadsheets/d/1gL-to78y5YGjDZMsHQ5w5hP4hC7D5kFZYmmY5BFqhdQ/edit#gid=0')
 
+    # при появлении любой ошибки записываем её в файл errors.log
     except Exception as e:
         logging.exception(e)
-        with open('errors.log', 'a') as f:
-            f.write(f'Time: {message.date}, Error: {str(e)}\n')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
